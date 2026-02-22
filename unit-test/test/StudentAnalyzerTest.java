bef1823bdd1b92a74021567b99d04116c6ec9961
kiet <kiet@codegym.vn>
2026-02-22 20:26:12 +0700

    test: add unit tests closes #3

---
+++ unit-test/test/StudentAnalyzerTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class StudentAnalyzerTest {
+
+    StudentAnalyzer analyzer = new StudentAnalyzer();
+
+    @Test
+    void testCount() {
+        assertEquals(2,
+            analyzer.countExcellentStudents(
+                Arrays.asList(9.0,8.5,7.0,11.0,-1.0)));
+    }
+
+    @Test
+    void testAverage() {
+        assertEquals(8.17,
+            analyzer.calculateValidAverage(
+                Arrays.asList(9.0,8.5,7.0,11.0,-1.0)),0.01);
+    }
+}
